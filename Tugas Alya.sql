7172e1781a6284d36977e49b310f2f67e017b5f0
Aris Candra <134421714+aris1c@users.noreply.github.com>
2025-06-18 20:09:52 +0700

    Tugas Alya Done

--- Tugas Alya.sql
+++ Tugas Alya.sql
@@ -34,3 +34,71 @@ where cs."Tenure_Months" > tm.rata_rata
 SELECT user_id,nama_lengkap,email,kota_asal,salary FROM alya_ds50;
 select * from alya_ds50
 ---kita upadate ?
+
+--Soal 1 
+select distinct "Product_Description"  from online_sales oe
+where cast("Transaction_Date" as TEXT) like '2019-07%'
+
+--Soal 2
+select "Product_Category", sum_Quantity from (select "Product_Category",sum ("Quantity")as sum_Quantity
+from ecommerce.online_sales 
+group by ("Product_Category")
+)
+where sum_Quantity > 20000
+
+---CTE (Common Table Expression)
+with Jumlah_Quantity as (
+select "Product_Category",sum ("Quantity")as sum_quantity
+from ecommerce.online_sales 
+group by ("Product_Category")
+)
+
+select "Product_Category", sum_quantity from Jumlah_Quantity
+where sum_quantity > 20000
+
+
+--Soal 3
+select avg("Quantity"*"Avg_Price")as gmv 
+from ecommerce.online_sales
+where "Product_Category" = 'Bags'
+
+--Soal 4
+
+with tm as(
+select "Location",avg ("Tenure_Months")as rata_rata from ecommerce.customers
+group by "Location"),
+
+ct as 
+(select * from ecommerce.customers)
+
+select ct."CustomerID",ct."Location" from tm 
+left join ct on ct."Location" = tm."Location"
+where ct."Tenure_Months" > tm.rata_rata
+
+
+--Soal 5
+  select distinct "Transaction_Date", "Location"
+  from (select * from ecommerce.online_sales t1
+  join ecommerce.customers t2
+  on t1."CustomerID" = t2."CustomerID")
+  where "Location" LIKE 'New%'
+
+
+--Commit Alya
+select * from ecommerce.online_sales
+where "Transaction_Date" like '2019-07%'
+
+--Commit Aris
+select * from ecommerce.customers
+
+
+
+
+
+
+
+
+
+
+
+
